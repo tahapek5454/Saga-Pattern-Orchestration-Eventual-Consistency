c661ab1d34b771c59b5522521b9c21c64a275045
tahapek5454 <tahapek5454@gmail.com>
2023-12-06 21:09:10 +0300

    build: build arch

---
+++ SagaStateMachine.Service/Program.cs
@@ -0,0 +1,10 @@
+using SagaStateMachine.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();

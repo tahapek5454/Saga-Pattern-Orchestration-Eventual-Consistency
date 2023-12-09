268f716e81439bd5a71173043893394e7bdc347c
tahapek5454 <tahapek5454@gmail.com>
2023-12-09 11:26:16 +0300

    feat: add queues

---
+++ Shared/Settings/RabbitMQSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Settings
+{
+    public static class RabbitMQSettings
+    {
+        public const string StateMachineQueue = "state-machine-queue";
+        public const string Stock_OrderCreatedEventQueue = "stock_order_created_event_queue";
+        public const string Order_OrderCompletedEventQueue = "order_order_completed_event_queue";
+        public const string Order_OrderFailedEventQueue = "order_order_completed_event_queue";
+        public const string Stock_RollbackMessageQueue = "stock_rollback_message_queue";
+        public const string Payment_PaymentStartedEventQueue = "payment_started_event_queue";
+    }
+}

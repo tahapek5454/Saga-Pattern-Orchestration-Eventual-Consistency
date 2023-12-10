8e20129ae09fe8f6051568bf3d82e6f5aee8f615
tahapek5454 <tahapek5454@gmail.com>
2023-12-10 11:34:03 +0300

    feat: add all consumers

---
+++ Order.API/Consumers/OrderCompletedEventConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Order.API.Context;
+using Shared.OrderEvents;
+
+namespace Order.API.Consumers
+{
+    public class OrderCompletedEventConsumer : IConsumer<OrderCompletedEvent>
+    {
+        private readonly OrderDbContext _context;
+
+        public OrderCompletedEventConsumer(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Consume(ConsumeContext<OrderCompletedEvent> context)
+        {
+            var order = _context.Orders.First(x => x.Id == context.Message.OrderId);
+
+            order.OrderStatus = Enums.OrderStatus.Completed;
+
+            _context.Update(order);
+
+            await _context.SaveChangesAsync();  
+        }
+    }
+}
---
+++ Order.API/Consumers/OrderFailedEventConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Order.API.Context;
+using Shared.OrderEvents;
+
+namespace Order.API.Consumers
+{
+    public class OrderFailedEventConsumer : IConsumer<OrderFailedEvent>
+    {
+        private readonly OrderDbContext _context;
+
+        public OrderFailedEventConsumer(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Consume(ConsumeContext<OrderFailedEvent> context)
+        {
+            var order = _context.Orders.First(x => x.Id == context.Message.OrderId);
+
+            order.OrderStatus = Enums.OrderStatus.Failed;
+
+            _context.Update(order);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}

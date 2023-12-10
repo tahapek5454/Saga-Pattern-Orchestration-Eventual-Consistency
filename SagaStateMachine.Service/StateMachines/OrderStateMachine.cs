d9ba54048ed14037537bb3b1eb625797def8ea97
tahapek5454 <tahapek5454@gmail.com>
2023-12-10 10:46:43 +0300

    feat: complete state machine

--- SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
+++ SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
@@ -1,7 +1,9 @@
 ﻿using MassTransit;
 using SagaStateMachine.Service.StateInstances;
+using Shared.Messages;
 using Shared.OrderEvents;
 using Shared.PaymentEvents;
+using Shared.Settings;
 using Shared.StockEvents;
 using System;
 using System.Collections.Generic;
@@ -20,6 +22,13 @@ namespace SagaStateMachine.Service.StateMachines
         public Event<StockNotReserved> StockNotReservedEvent { get; set; }
         public Event<PaymentFailedEvent> PaymentFailedEvent { get; set; }
 
+
+        // states
+        public State OrderCreated { get; set; }
+        public State StockReserved { get; set; }
+        public State StockNotReserved { get; set; }
+        public State PaymentCompleted { get; set; }
+        public State PaymentFailed { get; set; }
 
         public OrderStateMachine()
         {
@@ -64,6 +73,69 @@ namespace SagaStateMachine.Service.StateMachines
                 // not trigger event so we continue with was created CorrelationId we just bind Id 
                 orderStateInstance.CorrelateById(@event => @event.Message.CorrelationId);
             });
+
+
+            // when triger event incoming. First initially function work
+            // note: context.Instance is istance in database, contex.Data is incoming event data
+            Initially(When(OrderStartedEvent)
+                .Then(context =>
+                {
+                    context.Instance.OrderId = context.Data.OrderId;
+                    context.Instance.BuyerId = context.Data.BuyerId;
+                    context.Instance.TotalPrice = context.Data.TotalPrice;
+                })
+                .TransitionTo(OrderCreated)
+                .Send(new Uri($"queue:{RabbitMQSettings.Stock_OrderCreatedEventQueue}"), 
+                context => new OrderCreatedEvent(context.Instance.CorrelationId)
+                {
+                    OrderItems = context.Data.OrderItems,
+                }));
+
+            // incoming event after triger event - During function work 
+            During(OrderCreated, 
+                When(StockReservedEvent)
+                .TransitionTo(StockReserved)
+                .Send(new Uri($"queue:{RabbitMQSettings.Payment_PaymentStartedEventQueue}"),
+                context => new PaymentStartedEvent(context.Instance.CorrelationId)
+                {
+                    OrderItems = context.Data.OrderItems,
+                    TotalPrice = context.Data.OrderItems.Sum(x => x.Count * x.Price) // context.Instance.TotalPrice
+                }),
+                When(StockNotReservedEvent)
+                .TransitionTo(StockNotReserved)
+                .Send(new Uri($"queue:{RabbitMQSettings.Order_OrderFailedEventQueue}"),
+                context => new OrderFailedEvent()
+                {
+                    OrderId = context.Instance.OrderId,
+                    Message = context.Data.Message
+                }));
+
+
+            During(StockReserved,
+                When(PaymentCompletedEvent)
+                .TransitionTo(PaymentCompleted)
+                .Send(new Uri($"queue:{RabbitMQSettings.Order_OrderCompletedEventQueue}"),
+                context => new OrderCompletedEvent()
+                {
+                    OrderId = context.Instance.OrderId
+                })
+                .Finalize(), // final the sequence and delete stateInstance
+                When(PaymentFailedEvent)
+                .TransitionTo(PaymentFailed)
+                .Send(new Uri($"queue:{RabbitMQSettings.Order_OrderFailedEventQueue}"),
+                context => new OrderFailedEvent()
+                {
+                    OrderId = context.Instance.OrderId,
+                    Message = context.Data.Message
+                })
+                .Send(new Uri($"queue:{RabbitMQSettings.Order_OrderFailedEventQueue}"),
+                context => new StockRollbackMessage()
+                {
+                    OrderItems = context.Data.OrderItems
+                }));
+
+
+            SetCompletedWhenFinalized(); // final the sequence and delete stateInstance
         }
     }
 }
--- Shared/Settings/RabbitMQSettings.cs
+++ Shared/Settings/RabbitMQSettings.cs
@@ -11,8 +11,8 @@ namespace Shared.Settings
         public const string StateMachineQueue = "state-machine-queue";
         public const string Stock_OrderCreatedEventQueue = "stock_order_created_event_queue";
         public const string Order_OrderCompletedEventQueue = "order_order_completed_event_queue";
-        public const string Order_OrderFailedEventQueue = "order_order_completed_event_queue";
+        public const string Order_OrderFailedEventQueue = "order_order_failed_event_queue";
         public const string Stock_RollbackMessageQueue = "stock_rollback_message_queue";
-        public const string Payment_PaymentStartedEventQueue = "payment_started_event_queue";
+        public const string Payment_PaymentStartedEventQueue = "payment_payment_started_event_queue";
     }
 }
